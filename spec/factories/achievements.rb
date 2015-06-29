8ecf58efbc310c64988c4ef30e0c366b8db0e6a1
Rem Zolotykh <rem@zolotykh.net>
2015-06-29 03:02:59 +0200

    Factory girl and redcarpet

--- spec/factories/achievements.rb
+++ spec/factories/achievements.rb
@@ -1,10 +1,14 @@
 FactoryGirl.define do
   factory :achievement do
-    title "MyString"
-description "MyText"
-privacy 1
-featured false
-cover_image "MyString"
+    sequence(:title) { |n| "Achievement #{n}"}
+    description "description"
+    privacy Achievement.privacies[:private_access]
+    featured false
+    cover_image "some_file.png"
+
+    factory :public_achievement do
+      privacy Achievement.privacies[:public_access]
+    end
   end
 
 end
